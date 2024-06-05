fe69187536ac76dee6ec3a546ead2fc10cfce9bc
Deepanshu <goyaldeepanshu94@gmail.com>
2024-06-05 18:44:09 +0530

    renamed

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import ProfileComponent from "../../components/profile";
+import ProfileComponent from "../../components/profile/profile";
 
 const ProfileScreen = ({ navigation }) => {
   return <ProfileComponent navigation={navigation} />;
