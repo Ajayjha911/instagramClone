9369ce5ed8ab4c63260af078f1277064c609702e
Deepanshu <goyaldeepanshu94@gmail.com>
2024-06-05 18:18:59 +0530

    button comp added

---
+++ components/custom-button/custom-button.types.ts
@@ -0,0 +1,16 @@
+export declare type CustomButtonProps = {
+  onClick: () => void;
+  title: string;
+  textColor?: string;
+  textSize?: number;
+  backgroundColor?: string;
+  width?: number | string;
+  paddingTop?: number;
+  disabled?: boolean;
+  padding?: number;
+  loading?: boolean;
+  borderRadius?: number;
+  borderWidth?: number;
+  borderColor?: string;
+  textWeight?: string;
+};
