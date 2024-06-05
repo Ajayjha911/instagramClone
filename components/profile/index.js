1f5ba3fa165cb2edc3c7baca2ca78ad85e98229f
Deepanshu <goyaldeepanshu94@gmail.com>
2024-06-05 18:19:37 +0530

    updated ui

--- components/profile/index.js
+++ components/profile/index.js
@@ -8,13 +8,22 @@ import {
 } from "react-native";
 import styles from "./profile.style";
 import Icon from "react-native-vector-icons/FontAwesome5";
-import NewIcon from "react-native-vector-icons/FontAwesome6";
+import LockIcon from "react-native-vector-icons/Ionicons";
+import ThreadsIcon from "react-native-vector-icons/FontAwesome6";
+import PlusIcon from "react-native-vector-icons/Feather";
+
 import { ScrollView } from "react-native-gesture-handler";
 import { COLORS, SIZES, images } from "../../constants";
 import { USERS, POSTS } from "../../data";
 import ProfilePost from "./profile_post";
+import CustomButton from "../custom-button/custom-button";
+// import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
 const loggedInUser = USERS[1];
+
+const MyComp = () => {
+  return <Text>My Comp</Text>;
+};
 
 const ProfileScreen = ({ navigation }) => {
   const [activeTab, setActiveTab] = useState("posts");
@@ -23,10 +32,10 @@ const ProfileScreen = ({ navigation }) => {
     useMemo(() => {
       return {
         postsCreatedByLoggedInUser: (POSTS || [])?.filter(
-          (post) => post.userId === loggedInUser?.id
+          (post) => post.userId === loggedInUser?.id,
         ),
         postsLikedByLoggedInUser: (POSTS || []).filter((post) =>
-          post.likes.some((like) => like.userId === loggedInUser?.id)
+          post.likes.some((like) => like.userId === loggedInUser?.id),
         ),
       };
     }, [POSTS, loggedInUser]);
@@ -36,71 +45,98 @@ const ProfileScreen = ({ navigation }) => {
       activeTab === "posts"
         ? postsCreatedByLoggedInUser || []
         : postsLikedByLoggedInUser || [],
-    [activeTab, postsCreatedByLoggedInUser, postsLikedByLoggedInUser]
+    [activeTab, postsCreatedByLoggedInUser, postsLikedByLoggedInUser],
   );
+
+  // const Tab = createMaterialTopTabNavigator();
 
   return (
     <SafeAreaView style={styles.rootContainer}>
       <View style={styles.profileHeader}>
         <TouchableOpacity style={styles.disUserName}>
-          <Icon name="lock" size={20} />
-          <Text style={styles.userNameText}>user97</Text>
-          <Icon name="chevron-down" size={20} />
+          <LockIcon name="lock-closed-outline" size={16} />
+          <Text style={styles.userNameText}>deepanshu__goyal</Text>
+          <Icon name="chevron-down" size={16} />
         </TouchableOpacity>
         <View style={styles.profileIcons}>
-          <NewIcon name="at" size={30} />
-          <Icon name="plus-square" solid size={32} />
-          <Icon name="bars" size={30} />
+          <ThreadsIcon name="threads" size={24} />
+          <PlusIcon name="plus-square" solid size={24} />
+          <Icon name="bars" size={22} />
         </View>
       </View>
       <View style={styles.profileContainer}>
-        <View style={styles.imageContainer}>
-          <Image
-            source={loggedInUser?.profile}
-            resizeMode="contain"
-            style={styles.image}
-          />
+        <View>
+          <View style={styles.imageContainer}>
+            <Image
+              source={loggedInUser?.profile}
+              resizeMode="contain"
+              style={styles.image}
+            />
+          </View>
+          <Text style={styles.profileName}>Deepanshu Goyal</Text>
         </View>
+
         <View style={styles.countContainer}>
           <View style={styles.detailContainer}>
-            <Text style={styles.textLabel}>Posts</Text>
             <Text style={styles.textValue}>{loggedInUser.posts.length}</Text>
+            <Text style={styles.textLabel}>posts</Text>
           </View>
           <TouchableOpacity style={styles.detailContainer}>
-            <Text style={styles.textLabel}>Followers</Text>
             <Text style={styles.textValue}>
               {loggedInUser.followers.length}
             </Text>
+            <Text style={styles.textLabel}>followers</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.detailContainer}>
-            <Text style={styles.textLabel}>Following</Text>
             <Text style={styles.textValue}>
               {loggedInUser.following.length}
             </Text>
+            <Text style={styles.textLabel}>following</Text>
           </TouchableOpacity>
         </View>
       </View>
 
-      <View style={styles.bioContainer}>
-        <Text style={styles.textLabel}>{loggedInUser?.name}</Text>
-        <Text style={styles.textValue}>
-          {loggedInUser?.bio || "This is my bio!"}
-        </Text>
-      </View>
+      <Text style={styles.bio}>
+        my bio my bio my bio my bio my bio my bio my bio my bio
+        {/* {loggedInUser?.bio || "This is my bio!"} */}
+      </Text>
 
       <View style={styles.btnContainer}>
-        <TouchableOpacity style={styles.container}>
-          <Text style={styles.iconBtnText}>Edit Profile</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.container}>
-          <Text style={styles.iconBtnText}>Share Profile</Text>
-        </TouchableOpacity>
+        <CustomButton
+          title="Edit Profile"
+          onClick={() => {
+            //
+          }}
+          backgroundColor="#e5e7eb"
+          textColor="black"
+          textWeight="600"
+        />
+        <CustomButton
+          title="Share Profile"
+          onClick={() => {
+            //
+          }}
+          backgroundColor="#e5e7eb"
+          textColor="black"
+          textWeight="600"
+        />
         <TouchableOpacity style={styles.addFollowerContainer}>
-          <Icon name="user-plus" size={20} />
+          <Icon name="user-plus" size={16} />
         </TouchableOpacity>
       </View>
 
-      <View style={styles.iconContainer}>
+      {/* <Tab.Navigator>
+        <Tab.Screen name="Your lists" component={MyComp} />
+      </Tab.Navigator> */}
+
+      {/* <TopNav>
+        <Tab.Screen name="Your lists" component={MyComp} />
+        <Tab.Screen name="Saved lists" component={MyComp} />
+        <Tab.Screen name="Highlighted" component={MyComp} />
+        <Tab.Screen name="Reading history" component={MyComp} />
+      </TopNav> */}
+
+      {/* <View style={styles.iconContainer}>
         <TouchableOpacity
           style={styles.iconBtnContainer("posts", activeTab)}
           onPress={() => setActiveTab("posts")}
@@ -116,7 +152,7 @@ const ProfileScreen = ({ navigation }) => {
         >
           <Icon name="heart" size={25} solid />
         </TouchableOpacity>
-      </View>
+      </View> */}
 
       <>
         <ScrollView showsVerticalScrollIndicator={false}>
