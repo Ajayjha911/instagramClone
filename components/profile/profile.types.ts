b6cc3f9d85ebfcc6f67ac76b5aab75cad64d8a7b
Deepanshu <goyaldeepanshu94@gmail.com>
2024-06-07 13:49:01 +0530

    props updated

--- components/profile/profile.types.ts
+++ components/profile/profile.types.ts
@@ -1,3 +1,7 @@
+// import { UserState } from "@redux/slices/appSlice";
+
 export declare type ProfilePageProps = {
   isMyAccount: boolean;
+  activeUser: any;
+  handleBack?: () => void;
 };
--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,9 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import ProfileComponent from "../../components/profile/profile";
+import { USERS } from "data";
 
 const ProfileScreen = () => {
-  return <ProfileComponent isMyAccount={false} />;
+  const loggedInUser = USERS[1];
+
+  return <ProfileComponent isMyAccount={true} activeUser={loggedInUser} />;
 };
 
 export default ProfileScreen;
