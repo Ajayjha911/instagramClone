99360dd6d4f39581d01638e97a7ed98f8c56eb4b
Deepanshu <goyaldeepanshu94@gmail.com>
2024-06-07 18:46:05 +0530

    helpers added

--- src/helpers/func.ts
+++ src/helpers/func.ts
@@ -24,3 +24,20 @@ export const getFullDate = (value: string, onlyMonth = false) => {
     .toString()
     .padStart(2, "0")} ${monthName} ${date?.getFullYear()}`;
 };
+
+export const fresh = <T>(value: T): T => {
+  return JSON.parse(JSON.stringify(value));
+};
+
+export const generateUniqueId = (value?: string) => {
+  if (value) {
+    return (
+      value + " " + String(Math.floor(Math.random() * 1000000) + Date.now())
+    );
+  }
+  return String(Math.floor(Math.random() * 1000000) + Date.now());
+};
+
+export const emptyFunc = () => {
+  //void
+};
