a463be09a878e2c93e9266ce5b712f5b1d55b8ab
Deepanshu <goyaldeepanshu94@gmail.com>
2024-06-07 16:10:40 +0530

    updated

--- src/redux/slices/appSlice.ts
+++ src/redux/slices/appSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { AppRootState } from "..";
+import images from "@constants/images";
 
 export declare type AppState = {
   loginUser: UserState;
@@ -9,10 +10,9 @@ export declare type AppState = {
 export declare type UserState = {
   display_name?: string;
   user_name: string;
-  profile_image: string;
+  profile_image: any;
   id: string;
   bio?: string;
-  posts: string[];
   followers: string[];
   following: string[];
 };
@@ -22,9 +22,9 @@ export const dummyUsers: UserState[] = [
     id: "1",
     display_name: "Ajay Kumar",
     user_name: "__ajay_kumar",
-    profile_image: "https://loremflickr.com/640/480/dslr",
-    bio: "",
-    posts: ["0"],
+    profile_image: images.profile,
+    // profile_image: "https://loremflickr.com/640/480/dslr",
+    bio: "Welcome to Instagram Clone App",
     followers: ["2", "3", "4", "5"],
     following: ["2", "3", "4", "5"],
   },
@@ -32,9 +32,9 @@ export const dummyUsers: UserState[] = [
     id: "2",
     display_name: "Pankaj Kumar",
     user_name: "_pankaj_kumar",
-    profile_image: "https://loremflickr.com/640/480/dslr",
-    bio: "",
-    posts: ["0"],
+    profile_image: images.profile1,
+    // profile_image: "https://loremflickr.com/640/480/dslr",
+    bio: "Welcome to Instagram Clone App",
     followers: ["1", "4", "3"],
     following: ["3", "5"],
   },
@@ -42,9 +42,10 @@ export const dummyUsers: UserState[] = [
     id: "3",
     display_name: "Nischit Shetty",
     user_name: "nischit_shetty",
-    profile_image: "https://loremflickr.com/640/480/dslr",
-    bio: "",
-    posts: ["0"],
+    profile_image: images.profile2,
+
+    // profile_image: "https://loremflickr.com/640/480/dslr",
+    bio: "Welcome to Instagram Clone App",
     followers: ["1", "2", "5"],
     following: ["2", "1"],
   },
@@ -52,9 +53,10 @@ export const dummyUsers: UserState[] = [
     id: "4",
     display_name: "Rajesh",
     user_name: "rajesh_de_",
-    profile_image: "https://loremflickr.com/640/480/dslr",
-    bio: "",
-    posts: ["0"],
+    profile_image: images.profile3,
+
+    // profile_image: "https://loremflickr.com/640/480/dslr",
+    bio: "Welcome to Instagram Clone App",
     followers: ["1", "2", "5"],
     following: ["2", "1", "3"],
   },
@@ -62,9 +64,10 @@ export const dummyUsers: UserState[] = [
     id: "5",
     display_name: "John Doe",
     user_name: "doe_john_90",
-    profile_image: "https://loremflickr.com/640/480/dslr",
-    bio: "",
-    posts: ["0"],
+    profile_image: images.profile4,
+
+    // profile_image: "https://loremflickr.com/640/480/dslr",
+    bio: "Welcome to Instagram Clone App",
     followers: ["2", "3", "4"],
     following: ["2", "1"],
   },
@@ -72,9 +75,10 @@ export const dummyUsers: UserState[] = [
     id: "6",
     display_name: "Alisa",
     user_name: "alisa_30_10",
-    profile_image: "https://loremflickr.com/640/480/dslr",
-    bio: "",
-    posts: ["0"],
+    profile_image: images.profile5,
+
+    // profile_image: "https://loremflickr.com/640/480/dslr",
+    bio: "Welcome to Instagram Clone App",
     followers: ["4"],
     following: ["2", "1"],
   },
@@ -82,9 +86,10 @@ export const dummyUsers: UserState[] = [
     id: "7",
     display_name: "John Wick",
     user_name: "wick_john",
-    profile_image: "https://loremflickr.com/640/480/dslr",
-    bio: "",
-    posts: ["0"],
+    profile_image: images.profile,
+
+    // profile_image: "https://loremflickr.com/640/480/dslr",
+    bio: "Welcome to Instagram Clone App",
     followers: ["4", "1", "3", "5"],
     following: ["2", "1"],
   },
@@ -94,9 +99,9 @@ const loginUser = {
   id: "100",
   display_name: "Deepanshu Goyal",
   user_name: "deepanshu__goyal",
-  profile_image: "https://loremflickr.com/640/480/dslr",
-  bio: "This is dummy bio",
-  posts: ["0"],
+  profile_image: images.profile,
+  // profile_image: "https://loremflickr.com/640/480/dslr",
+  bio: "Welcome to Instagram Clone App",
   followers: ["1", "2", "3", "4", "5"],
   following: ["1", "2", "3", "4", "5"],
 };
